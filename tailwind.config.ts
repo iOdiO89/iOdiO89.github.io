d4f0b7f80383180fed0f669a8a62c4f3fe954967
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-06-20 01:00:45 +0900

    design: color 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        white: '#FFF',
+        grey: {
+          100: '#F2F2F7',
+          200: '#D1D1D6',
+          300: '#C7C7CC',
+          400: '#AEAEB2',
+          500: '#8E8E93',
+          600: '#636366',
+          700: '#48484A',
+          800: '#3A3A3C',
+          900: '#2C2C2E',
+        },
+        black: '#262626',
+        main: '#5E3BEE',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('tailwind-scrollbar-hide')],
+}
+export default config
